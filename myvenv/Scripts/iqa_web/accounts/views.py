9d05a998d6dfcfa2a6149c3fc9dd1b03c2d7959f
Klearez <krai020913@gmail.com>
2018-12-16 01:51:10 +0700

    SignUp
    - finish signup staff account

--- myvenv/Scripts/iqa_web/accounts/views.py
+++ myvenv/Scripts/iqa_web/accounts/views.py
@@ -13,7 +13,7 @@ def signup(request):
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 auth.login(request, user)
-                return redirect('home')
+                return render(request, 'accounts/login.html')
         else:
             return render(request, 'accounts/signup.html', {'error':'password is not match'})
     else:
